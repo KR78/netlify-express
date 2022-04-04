3b0ed06ab1825092eb86a090a84156c1bbf00a96
Victor Karanja <vic@vappsio.com>
2022-04-04 21:07:49 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,14 +1,11 @@
-const express = require("express");
+const proxy = require('./server.js');
+const express = require('express');
 const serverless = require("serverless-http");
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
-});
+router.get("/", proxy());
 
 app.use(`/.netlify/functions/api`, router);
 
